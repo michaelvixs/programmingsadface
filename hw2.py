4a609e77b6ffb3c8260d14bc062906470247e16d
Eren <michaelvixs@gmail.com>
2022-05-07 16:39:35 -0600

    feels

--- hw2.py
+++ hw2.py
@@ -1,7 +1,19 @@
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
+def classifer(string): 
+    pass
 
 def main():
-    pass
+    lines = [line.strip() for line in open('pos.train')]
+
+    poswords = []
+
+    for l in lines:
+
+        poswords.extend(l.split())
+    print(poswords)
 
 if __name__ == '__main__': 
     main()
+
