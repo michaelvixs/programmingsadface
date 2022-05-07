71cc9554a1e95f9d4b72c66f6adcb90d8ef33c20
superdaggler <ndbellew@gmail.com>
2022-05-07 17:47:09 -0600

    here you go

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -1,1 +1,22 @@
-# next
+
+def getData(File):
+    Vec = [[],[],[]]
+    with open(File) as f:
+        for line in f:
+            Vars = line.split()
+            GoldenValue = int(Vars[0]) # Golden Value
+            for i in range(1, len(Vars), 1):
+                if not Vec[GoldenValue]:
+                    Vec[GoldenValue] = []
+                Vec[GoldenValue].append(Vars[i].split(":")[0])
+    return Vec
+
+
+def main():
+    x = getData("hw3data/restaurant_train.txt")
+    print(x[0][0:10])
+
+
+
+if __name__ == "__main__":
+    main()
